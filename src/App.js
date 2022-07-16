eb667bc7d792ae7e4d254d8a9afb8140a1e2a465
Dane Kim <mr.dane@danes-mbp.lan>
2022-07-16 17:53:42 -0400

    wip:created and styled both login and register pages.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import NavBar from "./components/navbar/NavBar";
+import Register from "./pages/register/Register";
+// import Login from "./pages/login/Login";
 // import Home from "./pages/home/Home";
 // import Single from "./pages/single/Single";
 // import Write from "./pages/write/Write";
-import Settings from "./pages/settings/Settings";
+// import Settings from "./pages/settings/Settings";
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Settings />
+      <Register />
     </>
   );
 };
