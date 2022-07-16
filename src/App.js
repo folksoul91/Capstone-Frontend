b048599c876db20b3b9f9778428ac7b6f0b7450f
Dane Kim <mr.dane@danes-mbp.lan>
2022-07-16 11:33:14 -0400

    wip:created an add post page and styled the page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import NavBar from "./components/navbar/NavBar";
-import Home from "./pages/home/Home";
-import Single from "./pages/single/Single";
+// import Home from "./pages/home/Home";
+// import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Single />
+      <Write />
     </>
   );
 }
