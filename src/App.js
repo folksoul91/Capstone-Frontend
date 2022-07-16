90e68fbffe95dd8de8dbb30ab00e7020df4efe5d
Dane Kim <mr.dane@danes-mbp.lan>
2022-07-16 12:56:24 -0400

    wip:created a user settings page and the layout. Also styled the page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import NavBar from "./components/navbar/NavBar";
 // import Home from "./pages/home/Home";
 // import Single from "./pages/single/Single";
-import Write from "./pages/write/Write";
+// import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
-function App() {
+const App = () => {
   return (
     <>
       <NavBar />
-      <Write />
+      <Settings />
     </>
   );
-}
+};
 
 export default App;
